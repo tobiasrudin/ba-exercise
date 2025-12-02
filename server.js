080ca8930f6e0449562f3a2635976697419c3845
Tobias Rudin <tobias@Neptunus.local>
2025-12-02 12:21:23 +0100

    Added fixes for using websockets on Render

--- server.js
+++ server.js
@@ -3,9 +3,16 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use(express.static("public")); // serves index.html and assets
 
@@ -48,4 +55,8 @@ io.on("connection", socket => {
   });
 });
 
-server.listen(3000, () => console.log("Server running at http://localhost:3000"));
+// IMPORTANT: Render requires dynamic PORT
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
